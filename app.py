5b2fbf80f0971d57cc3feff546a1a26b63dfb2ff
sicluvbari <princeparrajuandavid@gmail.com>
2024-02-08 19:59:21 -0500

    sudoku python terminado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import SudokuFuncs
+import Sudoku
 import json
 
 
@@ -10,31 +10,39 @@ def mostrar_sudoku():
     if request.method == 'GET':
         # Obtener la lista enviada en el cuerpo del GET        
         # Devolver el JSON como respuesta
-        return SudokuFuncs.json_MyBoard
+        return Sudoku.board
 
-# @app.route('/sudoku', methods=['PUT'])
-# def añadir_numero_sudoku():
-#     if all(SudokuFuncs.isValid(board, row, col, num) for board in SudokuFuncs.myBoard
-#         for row in SudokuFuncs.myBoard for col in SudokuFuncs.myBoard for num in SudokuFuncs.myBoard):
-#         return SudokuFuncs.json_MyBoard 
-    
-        
+
+@app.route('/sudoku', methods=['POST'])
+def validar_sudoku():
+    data = request.get_json()  # Obtener los datos del cuerpo de la solicitud POST
+
+    # Validar el tablero de Sudoku
+    sudoku_validator = Sudoku.ValidateSudoku(data)
+    sudoku_validator.chequeo_general()
+    sudoku_validator.chequeo_filas()
+    sudoku_validator.chequeo_columnas()
+    sudoku_validator.chequeo_subcuadros()
+
+    # Si no se generó ninguna excepción, el tablero es válido
+    return jsonify({'mensaje': 'El tablero de Sudoku ingresado es válido'})
+
 #         # Devolver el tablero Sudoku actualizado
 #     return jsonify({"mensaje":"No se puede añadir, intenta con otro numero"}), 
 
-@app.route('/sudoku', methods=['PUT'])
-def añadir_numero_sudoku():
-    # Obtener los datos del JSON enviado en la solicitud
-    json_data = request.get_json()
-    #pasa el json a una lista 
+# @app.route('/sudoku', methods=['PUT'])
+# def añadir_numero_sudoku():
+#     # Obtener los datos del JSON enviado en la solicitud
+#     json_data = request.get_json()
+#     #pasa el json a una lista 
     
-    # Verificar si la posición es válida utilizando la función isValid()
-    if all(SudokuFuncs.isValid() for in json_data):        # La posición es válida, así que actualiza el tablero y devuelve el JSON actualizado
+#     # Verificar si la posición es válida utilizando la función isValid()
+#     if all(SudokuFuncs.isValid() for in json_data):        # La posición es válida, así que actualiza el tablero y devuelve el JSON actualizado
         
-        return json_data
+#         return json_data
     
-    # Si la posición no es válida, devuelve un mensaje de error
-    return jsonify({"mensaje": "No se puede añadir, intenta con otro número"}), 400
+#     # Si la posición no es válida, devuelve un mensaje de error
+#     return jsonify({"mensaje": "No se puede añadir, intenta con otro número"}), 400
 
     
 if __name__ == '__main__':
