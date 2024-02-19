12519075598390e6ef2775ee3188ac1e13278ce6
sicluvbari <princeparrajuandavid@gmail.com>
2024-02-18 22:36:41 -0500

    it werks

--- app.py
+++ app.py
@@ -1,8 +1,54 @@
+from azure.communication.email import EmailClient
 import json
 from flask import Flask, request, jsonify
 from Sudoku import convertir_tablero 
 from Sudoku import ValidateSudoku  
 app = Flask(__name__)
+
+def correo_mal():
+    try:
+        connection_string = "endpoint=https://prog3.unitedstates.communication.azure.com/;accesskey=26pmLQHFeg/sfCb7WlxkvbzngnOSeB9QzplyVXpJnG1hwCH+f8iLJ7QxE5au9vDGJcYmUM5fAt0o62tuGy4itQ=="
+        client = EmailClient.from_connection_string(connection_string)
+
+        message = {
+            "senderAddress": "DoNotReply@bd846568-501c-422a-ae4e-142a2e8da0b5.azurecomm.net",
+            "recipients":  {
+                "to": [{"address": "cachacoposting@gmail.com" }],
+            },
+            "content": {
+                "subject": "No se pudo ubicar",
+                "plainText": "“message”:”El dato no puede ser ubicado, cambie de posición”.",
+            }
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+
+    except Exception as ex:
+        print(ex)
+def correo_bien():
+    try:
+        connection_string = "endpoint=https://prog3.unitedstates.communication.azure.com/;accesskey=26pmLQHFeg/sfCb7WlxkvbzngnOSeB9QzplyVXpJnG1hwCH+f8iLJ7QxE5au9vDGJcYmUM5fAt0o62tuGy4itQ=="
+        client = EmailClient.from_connection_string(connection_string)
+
+        message = {
+            "senderAddress": "DoNotReply@bd846568-501c-422a-ae4e-142a2e8da0b5.azurecomm.net",
+            "recipients":  {
+                "to": [{"address": "cachacoposting@gmail.com" }],
+            },
+            "content": {
+                "subject": "Correo electrónico de prueba",
+                "plainText": "“message”:”Dato ubicado correctamente”",
+            }
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+
+    except Exception as ex:
+        print(ex)
+
+
 
 # Cargar el contenido del archivo ejemploTablero.json
 with open('ejemploTablero.json', 'r') as file:
@@ -38,9 +84,10 @@ def sudokucheckfilascolumnas():
         sudoku.chequeo_filas()
         sudoku.chequeo_columnas()
     except AssertionError as e:
+        correo_mal()
         return jsonify({"mensaje": str(e), "tablero": tablero_convertido}), 400
 
-
+    correo_bien()
     return jsonify({"mensaje": "El tablero de Sudoku ingresado es válido"}), 200
 
 @app.route('/sudokucheckcuadrante', methods=['POST'])
@@ -54,9 +101,10 @@ def sudokucheckcuadrante():
         sudoku.chequeo_general()
         sudoku.chequeo_subcuadros()
     except AssertionError as e:
+        correo_mal()
         return jsonify({"mensaje": str(e), "tablero": tablero_convertido}), 400
 
-
+    correo_bien()
     return jsonify({"mensaje": "Dato ubicado correctamente"}), 200
 
 if __name__ == '__main__':
